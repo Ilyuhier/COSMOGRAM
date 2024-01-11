a47500902e97c19b6aedd3af6db83dac295de0c7
Ilyuhier <illia.kharchenko.rs@gmail.com>
2024-01-11 20:46:36 +0200

    Дз 32

--- photos-app-main/js/main.js
+++ photos-app-main/js/main.js
@@ -1,7 +1,22 @@
-import { generateData } from "./dataGenerator.js";
-const photosArray = generateData()
 import { displayPictures } from "./displayPicture.js";
-displayPictures(photosArray)
+
+async function showData(){
+  try{
+    const response = await fetch('http://127.0.0.1:4026/photo')
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const photosArray = await response.json()
+    console.log(photosArray)
+    displayPictures(photosArray)
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
+showData()
 
 import { bigPicture } from "./bigPicture.js"
 document.querySelector('.pictures').addEventListener('click', checking)
@@ -13,4 +28,3 @@ function checking(evt){
 
 import { displayForm } from "./form.js";
 document.querySelector('#upload-file').addEventListener('change', displayForm)
-// displayForm()
